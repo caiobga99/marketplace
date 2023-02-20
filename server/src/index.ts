404ff290a35136281beba657166759f2901c2c25
caiobga99 <caiobga99@gmail.com>
2023-02-19 21:26:38 -0300

    configuring server setup with typescript.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import express from "express";
+import cors from "cors";
+const app: express.Application = express();
+const port: number = 8080;
+
+app.use(cors());
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`HTTP Server running on http://localhost/${port}`);
+});
