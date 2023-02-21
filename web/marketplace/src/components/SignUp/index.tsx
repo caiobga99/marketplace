7f49942b3febc42cc4df3a35470615aebafbb24e
caiobga99 <caiobga99@gmail.com>
2023-02-21 20:15:00 -0300

    Web - Adding login form component.

---
+++ web/marketplace/src/components/SignUp/index.tsx
@@ -0,0 +1,125 @@
+import { useForm } from "react-hook-form";
+import * as val from "validator";
+
+import "./style.css";
+
+interface Datas {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+  privacyTerms: boolean;
+}
+
+export const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const watchPassword = watch("password");
+
+  const onSubmit = (data: Datas) => {
+    alert(data);
+    console.log(data);
+  };
+
+  return (
+    <div className="app-container">
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          className={errors?.name && "input-error"}
+          type="text"
+          placeholder="Your name"
+          {...register("name", { required: true })}
+        />
+        {errors?.name?.type === "required" && (
+          <p className="error-message">Name is required.</p>
+        )}
+      </div>
+      <div className="form-group">
+        <label>E-mail</label>
+        <input
+          className={errors?.email && "input-error"}
+          type="email"
+          placeholder="Your e-mail"
+          {...register("email", {
+            required: true,
+            validate: (value) => val.default.isEmail(value),
+          })}
+        />
+        {errors?.email?.type === "required" && (
+          <p className="error-message">Email is required.</p>
+        )}
+
+        {errors?.email?.type === "validate" && (
+          <p className="error-message">Email is invalid.</p>
+        )}
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          className={errors?.password && "input-error"}
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: true, minLength: 7 })}
+        />
+
+        {errors?.password?.type === "required" && (
+          <p className="error-message">Password is required.</p>
+        )}
+
+        {errors?.password?.type === "minLength" && (
+          <p className="error-message">
+            Password needs to have at least 7 characters.
+          </p>
+        )}
+      </div>
+      <div className="form-group">
+        <label>Password confirmation</label>
+        <input
+          className={errors?.passwordConfirmation && "input-error"}
+          type="password"
+          placeholder="Repeat your password"
+          {...register("passwordConfirmation", {
+            required: true,
+            validate: (value) => value === watchPassword,
+          })}
+        />
+        {errors?.passwordConfirmation?.type === "required" && (
+          <p className="error-message">Password confirmation is required.</p>
+        )}
+
+        {errors?.passwordConfirmation?.type === "validate" && (
+          <p className="error-message">Passwords does not match.</p>
+        )}
+      </div>
+      <div className="form-group">
+        <div className="checkbox-group">
+          <input
+            type="checkbox"
+            {...register("privacyTerms", {
+              validate: (value) => value === true,
+            })}
+          />
+          <label>I agree with the privacy terms.</label>
+        </div>
+
+        {errors?.privacyTerms?.type === "validate" && (
+          <p className="error-message">
+            You must agree with the privacy terms.
+          </p>
+        )}
+      </div>
+      <div className="form-group">
+        <button onClick={() => handleSubmit(onSubmit as any)()}>
+          Create an Account
+        </button>
+      </div>
+      |
+    </div>
+  );
+};
