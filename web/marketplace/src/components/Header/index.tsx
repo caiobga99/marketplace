ff8e89a668b00868c6229505f17fb42f456c6663
caiobga99 <caiobga99@gmail.com>
2023-02-22 10:18:17 -0300

    Web - Adding a Header Component.

---
+++ web/marketplace/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export const Header = () => {
+  return (
+    <div className="header-container">
+      <div>Home</div>
+      <div>XXXXXXX</div>
+      <div>Sign In</div>
+      <div>Sign Up</div>
+    </div>
+  );
+};
