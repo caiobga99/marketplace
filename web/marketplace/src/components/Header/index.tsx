946e1fa7a900e3acbdbe9eaa3654a86339df6aa1
caiobga99 <caiobga99@gmail.com>
2023-02-23 15:10:38 -0300

    Web - Adding a Routes.

--- web/marketplace/src/components/Header/index.tsx
+++ web/marketplace/src/components/Header/index.tsx
@@ -1,12 +1,22 @@
+import { Link } from "react-router-dom";
+
 import "./style.css";
 
 export const Header = () => {
   return (
     <div className="header-container">
-      <div>Home</div>
-      <div>XXXXXXX</div>
-      <div>Sign In</div>
-      <div>Sign Up</div>
+      <Link to="#">
+        <div>Home</div>
+      </Link>
+      <Link to="#">
+        <div>XXXXXXX</div>
+      </Link>
+      <Link to="/signIn">
+        <div>Sign In</div>
+      </Link>
+      <Link to="/signUp">
+        <div>Sign Up</div>
+      </Link>
     </div>
   );
 };
