a2fbea6078c6420017ad7249dc20d71a3ed0a7cb
caiobga99 <caiobga99@gmail.com>
2023-02-24 18:12:40 -0300

    Web - Adding styles based on current URL.

--- web/marketplace/src/components/Header/index.tsx
+++ web/marketplace/src/components/Header/index.tsx
@@ -1,22 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "./style.css";
 
 export const Header = () => {
+  const location = useLocation();
+
+  const links = [
+    {
+      route: "/",
+      title: "Home",
+    },
+    {
+      route: "#",
+      title: "#",
+    },
+    {
+      route: "/signIn",
+      title: "Sign In",
+    },
+    {
+      route: "/signUp",
+      title: "Sign Up",
+    },
+  ];
+
   return (
     <div className="header-container">
-      <Link to="#">
-        <div>Home</div>
-      </Link>
-      <Link to="#">
-        <div>XXXXXXX</div>
-      </Link>
-      <Link to="/signIn">
-        <div>Sign In</div>
-      </Link>
-      <Link to="/signUp">
-        <div>Sign Up</div>
-      </Link>
+      {links.map((link) => {
+        return (
+          <Link
+            to={link.route}
+            className={location.pathname === link.route ? "currentUrl" : "link"}
+          >
+            <div>{link.title}</div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
