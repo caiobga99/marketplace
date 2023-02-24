d1edb362e2b6de8f38132351bf443775953091b3
caiobga99 <caiobga99@gmail.com>
2023-02-24 18:48:51 -0300

    Web - Adding a route of Home component.

--- web/marketplace/src/App.tsx
+++ web/marketplace/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { SignUp } from "./components/SignUp/index";
 import { SignIn } from "./components/SignIn/index";
+import { Home } from "./components/Home/index";
 
 export default function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/signIn" element={<SignIn />} />
         </Routes>
